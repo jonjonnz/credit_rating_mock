3bc3921beef736d332519d1a7f956152afb88533
jonjonnz <jsr7045@gmail.com>
2025-03-29 00:01:09 +0530

    added unit testing for main calculation function

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import credit_rating
 import json
 
-with open("input.json", "r") as f:
-    json_data = f.read()
 
-data = json.loads(json_data)
+def main():
+    with open("input.json", "r") as f:
+        json_data = f.read()
 
-rating = credit_rating.calculate_credit_rating(data.get("mortgages"))
+    data = json.loads(json_data)
 
-print(rating)
+    rating = credit_rating.calculate_credit_rating(data.get("mortgages"))
+
+    print(rating)
+
+
+if __name__ == "__main__":
+    main()
